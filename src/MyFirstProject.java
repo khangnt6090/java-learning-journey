d39b60a64e00505817bb06ba538ca1ec6a3f455c
khangnt6090 <noobvnkhang@gmail.com>
2026-04-23 16:55:01 +0700

    Thêm thư mục src và file MyFirstProject

---
+++ src/MyFirstProject.java
@@ -0,0 +1,10 @@
+public class MyFirstProject {
+    public static void main(String[] args){
+        // THIS IS MY FIRST JAVA PROGRAM!
+
+        System.out.print("I like pizza!\n");
+        System.out.print("It's very good!\n");
+        System.out.print("Buy me pizza!\n");
+        System.out.println("sout là lệnh in ra lệnh System.out.println");
+    }
+}
