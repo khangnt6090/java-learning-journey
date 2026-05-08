ffca4b8523f6f3217f7c237705453b1cbe1f5be6
khangnt6090 <noobvnkhang@gmail.com>
2026-05-08 16:29:29 +0700

    Thêm bài tập về printf

---
+++ src/Printf.java
@@ -0,0 +1,35 @@
+public class Printf {
+    public static void main(String[] args) {
+        //        String name = "Khang TG";
+//        char firstletter = 'K';
+//        int age = 20;
+//        double heigh = 174;
+//        boolean isEmployed = true;
+//
+//        System.out.printf("Hello %s\n", name);
+//        System.out.printf("Ten cua ban bat dau bang %c\n", firstletter);
+//        System.out.printf("Ban %d tuoi \n", age);
+//        System.out.printf("Ban cao %f cm\n", heigh);
+//        System.out.printf("Co nghe nghiep: %b\n", isEmployed);
+//
+//        System.out.printf("%s is %d years old", name, age);
+
+//        double price1 = 9.99;
+//        double price2 = 100.15;
+//        double price3 = -54.01;
+//
+//        System.out.printf("% .2f\n", price1);
+//        System.out.printf("% .2f\n", price2);
+//        System.out.printf("% .2f\n", price3);
+
+        int id1 = 1;
+        int id2 = 23;
+        int id3 = 456;
+        int id4 = 7890;
+
+        System.out.printf("%-4d\n", id1);
+        System.out.printf("%-4d\n", id2);
+        System.out.printf("%-4d\n", id3);
+        System.out.printf("%-4d\n", id4);
+    }
+}
